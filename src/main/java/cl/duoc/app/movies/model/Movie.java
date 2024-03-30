7873c885d81fc6041488a46f8befaeada20f9fba
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-30 01:13:05 -0300

    se modifica clase para ser usada como una entity

--- src/main/java/cl/duoc/app/movies/model/Movie.java
+++ src/main/java/cl/duoc/app/movies/model/Movie.java
@@ -4,23 +4,36 @@ package cl.duoc.app.movies.model;
     Las películas deben tener, al menos, los siguientes atributos: id, titulo, año,director, género y sinopsis.
  */
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "movie")
 @AllArgsConstructor //le indicamos a loombok que nos genere el constructor con parametros
+@NoArgsConstructor
 @Data //le decimos a loombok que se encargue de generar los get y set
 public class Movie {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "title")
     private  String title;
 
+    @Column(name = "year")
     private String year;
 
+    @Column(name = "director")
     private String director;
 
+    @Column(name = "genre")
     private String genre;
 
+    @Column(name = "synopsis")
     private String synopsis;
 
 }
