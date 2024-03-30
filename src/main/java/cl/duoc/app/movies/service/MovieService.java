5baf0aa1d3f4919a3db66f2432583637ff2c397e
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-30 01:20:36 -0300

    se cambia retorno de getMovie

--- src/main/java/cl/duoc/app/movies/service/MovieService.java
+++ src/main/java/cl/duoc/app/movies/service/MovieService.java
@@ -3,10 +3,12 @@ package cl.duoc.app.movies.service;
 import cl.duoc.app.movies.model.Movie;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieService {
 
-    Movie getMovie(int id);
+    Optional<Movie> getMovie(Long id);
 
     List<Movie> getMovies();
+
 }
