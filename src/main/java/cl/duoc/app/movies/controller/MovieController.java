8882b4f6259765b82cfbe26c2ce8e113bad8eac6
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-04 04:32:13 -0300

    se cambia lógica al momento de responder la información sobre las pelicuas, validando si es correcto, si no hay data en la bd o hay algún error al momento de hacer la consulta

--- src/main/java/cl/duoc/app/movies/controller/MovieController.java
+++ src/main/java/cl/duoc/app/movies/controller/MovieController.java
@@ -1,6 +1,7 @@
 package cl.duoc.app.movies.controller;
 
 import cl.duoc.app.movies.model.Movie;
+import cl.duoc.app.movies.model.MovieResponse;
 import cl.duoc.app.movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,8 +30,24 @@ public class MovieController {
     }
 
     @GetMapping("/peliculas")
-    public ResponseEntity<List<Movie>> getMovies() {
-        return ResponseEntity.ok(movieService.getMovies());
+    public ResponseEntity<List<MovieResponse>> getMovies() {
+        try {
+            //se obtienen las peliculas desde la bd
+            List<Movie> movies = movieService.getMovies();
+            //se evalua si la lista trae informacion
+            if (null != movies && !movies.isEmpty()) {
+                //se retorna la informacion correcta
+                return ResponseEntity.ok(Collections.singletonList(
+                        new MovieResponse("Success", movies)));
+            }
+            // se retorna indicando que no se encontraron peliculas
+            return ResponseEntity.ofNullable(Collections.singletonList(
+                    new MovieResponse("No se encontraron peliculas en la bd", movies)));
+        } catch (Exception e) {
+            // en caso de excepcion se retorna indicando que no se pudo obtener las peliculas con el error asociado
+            return (ResponseEntity.internalServerError().body(Collections.singletonList(
+                    new MovieResponse("Error al obntener pelicuas desde la bd: " + e.getMessage(), null))));
+        }
     }
 
 }
