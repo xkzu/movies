deaa2bc5c1c357c4a25756cd0834fa22f50cbf6e
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-21 04:23:07 -0400

    se modifican todos los endpoints para agregar hateoas

--- src/main/java/cl/duoc/app/movies/controller/MovieController.java
+++ src/main/java/cl/duoc/app/movies/controller/MovieController.java
@@ -2,17 +2,31 @@ package cl.duoc.app.movies.controller;
 
 import cl.duoc.app.movies.model.Movie;
 import cl.duoc.app.movies.model.MovieResponse;
-import cl.duoc.app.movies.model.MoviesResponse;
 import cl.duoc.app.movies.service.MovieService;
 import cl.duoc.app.movies.util.MovieUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 
-import java.util.Collections;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController //le indicamos a springboot que maneje esta clase como RestController
 public class MovieController {
@@ -25,6 +39,7 @@ public class MovieController {
         this.movieService = movieService;
     }
 
+/*
     @GetMapping("/peliculas/{id}")
     public ResponseEntity<Optional<MovieResponse>> getMovie(@PathVariable Long id) {
         //se evalua si el id ingresado es menor a 1
@@ -51,7 +66,32 @@ public class MovieController {
                     new MovieResponse("Error al obntener la pelicula desde la bd: " + e.getMessage(), null)));
         }
     }
+*/
 
+    // No me gustó hateoas :c
+    @GetMapping("/peliculas/{id}")
+    public ResponseEntity<EntityModel<MovieResponse>> getMovie(@PathVariable Long id) {
+        if (id < 1) {
+            MovieResponse response = new MovieResponse("El id ingresado debe ser mayor a cero", Optional.empty());
+            return ResponseEntity.badRequest().body(EntityModel.of(response));
+        }
+        try {
+            Optional<Movie> movie = movieService.getMovie(id);
+            MovieResponse response = new MovieResponse("Success", movie);
+            if (movie.isPresent()) {
+                EntityModel<MovieResponse> model = EntityModel.of(response,
+                        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getMovie(id)).withSelfRel());
+                return ResponseEntity.ok(model);
+            } else {
+                return ResponseEntity.ok(EntityModel.of(response));
+            }
+        } catch (Exception e) {
+            MovieResponse errorResponse = new MovieResponse("Error al obtener la película desde la bd: " + e.getMessage(), Optional.empty());
+            return ResponseEntity.internalServerError().body(EntityModel.of(errorResponse));
+        }
+    }
+
+/*
     @GetMapping("/peliculas")
     public ResponseEntity<List<MoviesResponse>> getMovies() {
         try {
@@ -72,7 +112,66 @@ public class MovieController {
                     new MoviesResponse("Error al obntener pelicuas desde la bd: " + e.getMessage(), null))));
         }
     }
+*/
 
+    @GetMapping("/peliculas")
+    public ResponseEntity<CollectionModel<EntityModel<Movie>>> getMovies() {
+        try {
+            List<Movie> movies = movieService.getMovies();
+            if (movies.isEmpty()) {
+                return ResponseEntity.ok(CollectionModel.empty());
+            }
+
+            List<EntityModel<Movie>> movieModels = movies.stream()
+                    .map(movie -> EntityModel.of(movie,
+                            linkTo(methodOn(this.getClass()).getMovie(movie.getId())).withSelfRel()))
+                    .collect(Collectors.toList());
+
+            CollectionModel<EntityModel<Movie>> collectionModel = CollectionModel.of(movieModels,
+                    linkTo(methodOn(this.getClass()).getMovies()).withSelfRel());
+
+            return ResponseEntity.ok(collectionModel);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/peliculas/agregar")
+    public ResponseEntity<EntityModel<Map<String, Object>>> addMovie(@RequestBody Movie movie) {
+        try {
+            if (MovieUtil.isEmptyOrNull(movie.getTitle()) ||
+                    MovieUtil.isEmptyOrNull(movie.getYear()) ||
+                    MovieUtil.isEmptyOrNull(movie.getDirector()) ||
+                    MovieUtil.isEmptyOrNull(movie.getGenre()) ||
+                    MovieUtil.isEmptyOrNull(movie.getSynopsis())) {
+                return ResponseEntity.badRequest().body(EntityModel.of(Map.of(
+                        "message", "title, year, director, genre y synopsis no pueden ser nulos ni vacios")));
+            }
+
+            Movie savedMovie = movieService.addMovie(movie);
+            Map<String, Object> responseContent = new HashMap<>();
+            responseContent.put("id", savedMovie.getId());
+            responseContent.put("title", savedMovie.getTitle());
+            responseContent.put("year", savedMovie.getYear());
+            responseContent.put("director", savedMovie.getDirector());
+            responseContent.put("genre", savedMovie.getGenre());
+            responseContent.put("synopsis", savedMovie.getSynopsis());
+
+            EntityModel<Map<String, Object>> entityModel = EntityModel.of(
+                    Map.of("Pelicula insertada", responseContent),
+                    linkTo(methodOn(this.getClass()).getMovie(savedMovie.getId())).withRel("get-this-movie"),
+                    linkTo(methodOn(this.getClass()).getMovies()).withRel("all-movies"));
+
+            return ResponseEntity.ok(entityModel);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(EntityModel.of(Map.of(
+                    "message", "Error al insertar pelicula: " + e.getMessage())));
+        }
+    }
+
+
+
+/*
     @PostMapping("/peliculas/agregar")
     public ResponseEntity<Map<String, String>> addMovie(@RequestBody Movie movie) {
         try {
@@ -95,7 +194,40 @@ public class MovieController {
             return ResponseEntity.internalServerError().body(Map.of("Error al insertar pelicula", e.getMessage()));
         }
     }
+*/
 
+    @PutMapping("/peliculas/actualizar")
+    public ResponseEntity<EntityModel<Map<String, String>>> updateMovie(@RequestBody Movie movie) {
+        try {
+            if (movie.getId() == null) {
+                return ResponseEntity.badRequest().body(EntityModel.of(Map.of("El id ingresado no puede ser null", movie.toString())));
+            }
+
+            Optional<Movie> validateMovie = movieService.getMovie(movie.getId());
+            if (validateMovie.isEmpty()) {
+                return ResponseEntity.badRequest().body(EntityModel.of(Map.of("El id de la pelicula ingresada no existe en la bd", movie.toString())));
+            }
+
+            if (MovieUtil.isEmptyOrNull(movie.getTitle()) || MovieUtil.isEmptyOrNull(movie.getYear())
+                    || MovieUtil.isEmptyOrNull(movie.getDirector()) || MovieUtil.isEmptyOrNull(movie.getGenre())
+                    || MovieUtil.isEmptyOrNull(movie.getSynopsis())) {
+                return ResponseEntity.badRequest().body(EntityModel.of(Map.of("title, year, director, genre y synopsis no pueden ser nulos ni vacios", movie.toString())));
+            }
+
+            movieService.updateMovie(movie);
+            Map<String, String> response = new HashMap<>();
+            response.put("Pelicula actualizada", movie.toString());
+            EntityModel<Map<String, String>> entityModel = EntityModel.of(response,
+                    linkTo(methodOn(this.getClass()).getMovie(movie.getId())).withRel("get-this-movie"),
+                    linkTo(methodOn(this.getClass()).getMovies()).withRel("all-movies"));
+
+            return ResponseEntity.ok(entityModel);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(EntityModel.of(Map.of("Error al actualizar la pelicula", e.getMessage())));
+        }
+    }
+
+/*
     @PutMapping("/peliculas/actualizar")
     public ResponseEntity<Map<String, String>> updateMovie(@RequestBody Movie movie) {
         try {
@@ -132,7 +264,28 @@ public class MovieController {
         }
 
     }
+*/
 
+    @DeleteMapping("/peliculas/eliminar/{id}")
+    public ResponseEntity<EntityModel<Map<String, String>>> deleteMovie(@PathVariable Long id) {
+        if (id == null) {
+            return ResponseEntity.badRequest().body(EntityModel.of(Map.of("El id ingresado no puede ser null", "null")));
+        }
+
+        Optional<Movie> movie = movieService.getMovie(id);
+        if (movie.isEmpty()) {
+            return ResponseEntity.badRequest().body(EntityModel.of(Map.of("El id de la pelicula ingresada no existe en la bd", "ID: " + id)));
+        }
+
+        movieService.deleteMovie(id);
+        Map<String, String> response = Map.of("Pelicula eliminada", "ID: " + id);
+        EntityModel<Map<String, String>> entityModel = EntityModel.of(response,
+                linkTo(methodOn(this.getClass()).getMovies()).withRel("all-movies"));
+
+        return ResponseEntity.ok(entityModel);
+    }
+
+/*
     @DeleteMapping("peliculas/eliminar/{id}")
     public ResponseEntity<Map<String, String>> deleteMovie(@PathVariable Long id) {
         try {
@@ -153,5 +306,6 @@ public class MovieController {
             return ResponseEntity.internalServerError().body(Map.of("Error al actualizar la pelicula", e.getMessage()));
         }
     }
+*/
 
 }
